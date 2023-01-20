a0a8d2dabe6692764585fca023e600c3839ef52b
Josué <gutillaj@gmail.com>
2023-01-20 16:08:16 -0400

    projeto funcionario

---
+++ Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_funcionario
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+        public double SalarioLiquido()
+        {
+            double salario = SalarioBruto - Imposto;
+
+            return salario;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ex_funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario(); //criando a variavel da classe e instanciando
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + funcionario);
+
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double aumentarSal = double.Parse(Console.ReadLine());
+            funcionario.AumentarSalario(aumentarSal);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + funcionario);
+        }
+    }
+}
